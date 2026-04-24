453223d16db0894955ea3a4def2b6bfc52db683c
lyndon <xieliangkk@outlook.com>
2026-04-24 22:58:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'bloombergnf.x5411y.cloudns.biz';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiZTcyNjljODctYWVkZC00OWY5LTliNDctZDIxNzFjOTQxOWUzIiwicyI6Ik5UWTJPR1l4TkdZdE9XSmpNaTAwWWpNMExUZzJZell0TmpKbE1ETXhNVFZsWlRGaCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
-const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
+const ARGO_PORT = process.env.ARGO_PORT || 8081;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
+const CFIP = process.env.CFIP || 'www.visa.usa';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'NF-us';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
